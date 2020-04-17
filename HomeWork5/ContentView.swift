7f2a86e5cfa2921a803472a0473df4b52b9aa877
Vitaliy Voronok <vitalii.voronok@skylum.com>
2020-04-17 13:35:57 +0300

    Initial Commit

---
+++ HomeWork5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeWork5
+//
+//  Created by Vitaliy Voronok on 17.04.2020.
+//  Copyright © 2020 Vitaliy Voronok. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
